a0ea186236e8bb3dd3b0baa8ca3c4aa821a22dbb
unknown <enter7377@naver.com>
2022-06-02 00:00:14 +0900

    reaf: remove useless imports

--- lib/screens/all.dart
+++ lib/screens/all.dart
@@ -1,7 +1,6 @@
 // ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
 
 import 'dart:async';
-import 'dart:convert';
 
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
